4d53ea7f4cd07e2dc5e73a01e758b957858cebd8
moyosoreabiodun <97528716+moyosoreabiodun@users.noreply.github.com>
2025-02-03 15:14:43 +0100

    Delete Task 0 - Responsive Profile Card directory
    I uploaded the local folder but instead I want to upload the files in the folder to the git folder/repo i already created here.

--- Task 0 - Responsive Profile Card/index.js
+++
@@ -1,7 +0,0 @@
-function getCurrentUTCTime() {
-    const currentDate = new Date();
-    const currentUTCTime = currentDate.toUTCString();
-    return currentUTCTime;
-}
-
-document.getElementById("current-time").innerText = getCurrentUTCTime();
